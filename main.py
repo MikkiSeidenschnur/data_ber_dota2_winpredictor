6147a1244bfd8a4c2828613dc7b4c436d65bb956
fs1m <edgar1991@gmail.com>
2020-07-07 09:15:14 +0200

    enhanced miner loop

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ import pandas as pd
 id_list = pd.read_csv("matchesdata/matches_ids.csv")
 id_list = id_list["match_id"].values.tolist()
 
-mm.get_matches_data(id_list, "matchesdata/")
+
+# adjust to start where you want.
+from_position = 0
+until_position = 5000
+
+while from_position < until_position:
+    try:
+        mm.get_matches_data(
+            id_list[from_position:until_position], "matchesdata/")
+        from_position += 1
+    except:
+        print("trying again...")
+        time.sleep(5)
